05b8ce7ed48210f17525ac5ec5f0bb53fef3a3cc
gui machiavelli <hey@guimachiavelli.com>
2013-02-28 03:36:36 +0100

    small css fixes for open/close work + title now aligning according to instructions

--- js/script.js
+++ js/script.js
@@ -8,13 +8,19 @@ $(document).ready(function () {
 	
 	$('.media-nav h1').click(function() {
 		$('ol').addClass('inactive');
+		$('.media-nav h1').removeClass('active');
 		$(this).siblings('ol').removeClass('inactive');
+		$(this).addClass('active');
+
 		return false;
 	});
+$('.fullscreen-image').click(function() {
+	return false
 
+});
 
 	$('.media-nav li').click(function() {
-		var target;
+		var target, test;
 
 		target = $(this).children('.hidden-content');
 		test = target[0].innerHTML;
