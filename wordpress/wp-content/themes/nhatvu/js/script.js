c2be22263d9a6c941ed2ac5fc7a1c231ae7871bc
gui machiavelli <hey@guimachiavelli.com>
2013-03-18 21:41:12 +0100

    number of current picture is now highlighted on front page

--- wordpress/wp-content/themes/nhatvu/js/script.js
+++ wordpress/wp-content/themes/nhatvu/js/script.js
@@ -18,6 +18,8 @@ $(document).ready(function () {
 
 	$('.media-nav li').click(function() {
 		var target, test;
+		
+		$(this).addClass('active').siblings('li').removeClass('active');
 
 		target = $(this).children('.hidden-content');
 		test = target[0].innerHTML;
