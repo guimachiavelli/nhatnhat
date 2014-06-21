4a09351fd0ea5fe222b889c837e4e0004a3bdda3
gui machiavelli <hey@guimachiavelli.com>
2014-06-21 20:46:19 +0100

    more tweaks

--- wordpress/wp-content/themes/nhatvu/header.php
+++ wordpress/wp-content/themes/nhatvu/header.php
@@ -5,13 +5,13 @@
 <!--[if gt IE 8]><!--> <html class="no-js <?php echo print_classes(); ?>"> <!--<![endif]-->
 	<head>
 		<meta charset="utf-8">
-	
+
 		<title><?php bloginfo('name'); ?></title>
 		<meta name="description" content="">
-	
+
 		<meta name="viewport" content="width=device-width">
 		<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-	
+
 		<script src="<?php echo TEMPLATE_URL; ?>/js/libs/modernizr.js"></script>
 
         <link rel="profile" href="http://gmpg.org/xfn/11">
@@ -25,7 +25,7 @@
 		<header id="intro" role="banner">
 			<a href="<?php echo SITE_URL; ?>">
 				<hgroup>
-					<h1 class="name"><b class="nhat">Nhat</b> <b class="dash">–</b> <b class="vu">Vu</b> <b class="dang">Dang</b></h1>
+					<h1 class="name written"><b class="nhat">Nhat</b> <b class="dash">–</b> <b class="vu">Vu</b> <b class="dang">Dang</b></h1>
 					<h2 class="name pronunciation"><b class="nhat">(Njutt)</b> <b class="vu">(Foo)</b> <b class="dang">(Dang)</b></h2>
 				</hgroup>
 			</a>
